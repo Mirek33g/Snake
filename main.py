3c568780d9ac3792a88065ad45020d6545786e6a
Mirek33g <133931969+Mirek33g@users.noreply.github.com>
2023-07-06 22:55:42 +0000

    Snake positions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen.setup(width= 600, height= 600)
 screen.bgcolor("black")
 screen.title("My Snake game")
 
-
+#list of snakes positions
 turtle_position = [(0, 0),(-20, 0),(-40, 0)]
 new_obj = []
 
